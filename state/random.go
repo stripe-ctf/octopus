b9d0654f2394c601b6ef7f0ffb623bbed4d04057
Carl Jackson <carl@stripe.com>
2014-01-25 16:43:02 -0800

    Actually generate random numbers
    This fixes a bug where ~all agents in Octopus were being seeded (nearly?)
    deterministically from fixed strings due to a bug in the usage of the hash.Hash
    API.

--- state/random.go
+++ state/random.go
@@ -2,22 +2,25 @@ package state
 
 import (
 	"bytes"
+	realrand "crypto/rand"
 	"crypto/sha1"
-	"encoding/binary"
 	"fmt"
 	"github.com/stripe-ctf/octopus/log"
+	"math"
+	"math/big"
 	"math/rand"
 )
 
 func NewRand(name string) *rand.Rand {
 	source := fmt.Sprintf("%s-%d", name, Seed())
-	image := sha1.New().Sum([]byte(source))
-	b := bytes.NewReader(image)
-	derived, err := binary.ReadVarint(b)
+	image := sha1.Sum([]byte(source))
+	b := bytes.NewReader(image[:])
+	n, err := realrand.Int(b, big.NewInt(math.MaxInt64))
 	if err != nil {
 		log.Fatalf("Bug in seed derivation: %s", err)
 	}
-	return rand.New(rand.NewSource(derived))
+	log.Debugf("Seed for %v (source %v) is %v", name, source, n.Int64())
+	return rand.New(rand.NewSource(n.Int64()))
 }
 
 var alphabet = "abcdefghijkmnpqrstuvwxyz01234567890"
